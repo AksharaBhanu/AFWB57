2520ffa26c1b22a89270579fc1f3083cbe94dd44
AksharaBhanu <javagalbhanu@gmail.com>
2022-11-08 20:35:01 +0530

    H

---
+++ pages/login_page.py
@@ -0,0 +1,31 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+
+class LoginPage:
+
+    __username=(By.ID,"username")
+    __password=(By.NAME,"pwd")
+    __loginbutton=(By.XPATH,"//div[.='Login ']")
+    __errmsg=(By.XPATH,"//span[contains(text(),'invalid')]")
+
+    def __init__(self,driver):
+        self.__driver=driver
+
+    def set_username(self,un):
+        self.__driver.find_element(*self.__username).send_keys(un)
+
+    def set_password(self,pw):
+        self.__driver.find_element(*self.__password).send_keys(pw)
+
+    def click_loginbutton(self):
+        self.__driver.find_element(*self.__loginbutton).click()
+
+    def verify_errmsg_displayed(self,wait):
+        try:
+            wait.until(EC.visibility_of_element_located(self.__errmsg))
+            print("Err Msg is displayed")
+            return True
+
+        except:
+            print("Err Msg is not displayed")
+            return False
